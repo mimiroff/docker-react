c4afdae77733a811897bbd547d56e15a8f927b24
Ilia Barashkov <ibarashkov@list.ru>
2019-01-18 22:06:39 +0300

    minor change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Bye there!</p>
+          <p>Hi there!</p>
           <a
             className="App-link"
             href="https://reactjs.org"
